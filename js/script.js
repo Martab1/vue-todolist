2be2f14b45b4811dbbe5e8059cc6293bf4bf8ae1
Martab1 <martabernardo86@gmail.com>
2021-04-22 15:47:36 +0200

    create repo & layout

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*  TO DO LIST
+- stampare in pagina un item per ogni elemento contenuto in un array di todo
+- predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+- ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+*/
